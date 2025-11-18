3569779f3037547cf643706841f6a491592d1890
arseniji <odiousguyfromnet@gmail.com>
2025-11-18 19:54:58 +0300

    commit task 2 1

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main(){
-    string kigger;
-    cout << "Say your name";
-    cin >> kigger;
+    std::string kigger;
+    std::cout << "Say your name";
+    std::cin >> kigger;
 
     std::cout << "Hello_world from" << kigger << endl;
 }
