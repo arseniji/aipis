12ba74e459ebc8699c0b06c3729ddee8ab748eec
arseniji <odiousguyfromnet@gmail.com>
2025-11-18 20:29:07 +0300

    mozila stil

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 
-int main(){ // мейн
-    std::string kigger; //перменая
-    std::cout << "Say your name";//выод
-    std::cin >> kigger;//вод
+int
+main()
+{                               // мейн
+  std::string kigger;           // перменая
+  std::cout << "Say your name"; // выод
+  std::cin >> kigger;           // вод
 
-    std::cout << "Hello_world from" << kigger << endl;//выод привет
+  std::cout << "Hello_world from" << kigger << endl; // выод привет
 }
