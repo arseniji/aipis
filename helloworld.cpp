c255344e17bdba02a399766767261390518ba448
arseniji <odiousguyfromnet@gmail.com>
2025-11-18 19:58:40 +0300

    add comments

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 
-int main(){
-    std::string kigger;
-    std::cout << "Say your name";
-    std::cin >> kigger;
+int main(){ // мейн
+    std::string kigger; //перменая
+    std::cout << "Say your name";//выод
+    std::cin >> kigger;//вод
 
-    std::cout << "Hello_world from" << kigger << endl;
+    std::cout << "Hello_world from" << kigger << endl;//выод привет
 }
